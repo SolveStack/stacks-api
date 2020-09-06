a0a75845b47e9f96a8242563ceef80aebd3abd01
Tom Boolean <tomboolean@gmail.com>
2020-09-05 22:39:18 -0700

    stacks post, start of stacks api

---
+++ src/components/Stack/index.ts
@@ -0,0 +1,25 @@
+import StackService from "./service";
+import { HttpError } from "../../config/error";
+import { IStackModel } from "./model";
+import { NextFunction, Request, Response } from "express";
+
+/**
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ * @returns {Promise < void >}
+ */
+export async function create(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const user: IStackModel = await StackService.insert(req.body);
+
+    res.status(201).json(user);
+  } catch (error) {
+    next(new HttpError(error.message.status, error.message));
+  }
+}
---
+++ src/routes/StackRouter.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { UserComponent, StackComponent } from "../components";
+
+/**
+ * @constant {express.Router}
+ */
+const router: Router = Router();
+
+/**
+ * POST method route
+ * @example http://localhost:PORT/v1/stacks
+ */
+router.post("/", StackComponent.create);
+
+/**
+ * @export {express.Router}
+ */
+export default router;
