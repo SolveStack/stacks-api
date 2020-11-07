bc83c640a46d10f015b8c2412b6c76ab3e23295b
Mike Cho <elegantspanishguitar@gmail.com>
2020-11-07 16:50:59 -0700

    added tests

--- test/api.js
+++ test/api.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const request = require('supertest');
 const app = require('../src/config/server/server').default;
 const UserModel = require('../src/components/User/model').default;
+const StackModel = require('../src/components/Stack/model').default;
 chai.should();
 
 /**
@@ -19,12 +20,22 @@ describe('API', () => {
             .end(done);
     });
 
+    it('get all stacks', (done) => {
+        request(app)
+            .get('/v1/stacks')
+            .set('x-access-token', global.token)
+            .expect((res) => {
+                res.status.should.equal(200);
+                res.body.should.be.an('array');
+            })
+            .end(done);
+    });
+
     it('create new user', (done) => {
         const newUser = {
             email: 'new.user@gmail.com',
             name: 'John Doe'
         };
-
         request(app)
             .post('/v1/users')
             .send(newUser)
@@ -32,6 +43,22 @@ describe('API', () => {
             .expect((res) => {
                 res.status.should.equal(201);
                 res.body.should.have.property('email');
+            })
+            .end(done);
+    });
+
+    it('create new stack', (done) => {
+        const newStack = {
+            name: 'stack-name',
+            wikipediaLink: 'https://sticksftw.com'
+        };
+        request(app)
+            .post('/v1/stacks')
+            .send(newStack)
+            .set('x-access-token', global.token)
+            .expect((res) => {
+                res.status.should.equal(201);
+                res.body.should.have.property('name');
             })
             .end(done);
     });
